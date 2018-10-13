8b81142785783705b0e38f4367cf3ce5dcb1c7cc
Christoph Kastius <kastius@gmail.com>
2018-10-13 16:54:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
-		<title>Meine IPv6</title>
+		<title>Meine IPv4/IPv6</title>
 
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 		<meta name="keywords" content="ip,ipv6,host,hostname,tcp,adresse,ip-adresse,DNS-Zone.NET"/>
